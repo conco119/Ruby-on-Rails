3156172449fbe55bdc612a8f32e31910743ebed1
Macgit <tienduy2396@gmail.com>
2017-04-26 14:24:18 +0700

    chap7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      redirect_to @user
+      flash[:success] = t ".welcome"
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+    unless @user
+      flash[:danger] = t ".error_flash"
+      redirect_to root_path
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,17 +6,11 @@ class UserTest < ActiveSupport::TestCase
     password: "foorbar", password_confirmation: "foobar")
   end
 
-  test "email address should be saved as lower-case" do
-    @user.email = "USER@example.com"
-    @user.save
-    assert_equal "user@example.com", @user.email , "Email da luu la #{@user.email}"
-  end
-
   test "password should be presence" do
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
   end
-  
+
   test "password should have minium length" do
     @user.password = @user.password_confirmation = "a" * 4
     assert_not @user.valid?
